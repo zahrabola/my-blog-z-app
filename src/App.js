3e8e7d09a958e6b44a736765c962bcb0a23b3bad
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-25 14:30:50 +0100

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-///import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-     <div>Zahra Olanrewaju</div>
+      <div>Zahra Olanrewaju</div>
+<Navbar/>
+      
     </div>
   );
 }
