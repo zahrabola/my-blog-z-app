80f914585e7d0ed45fec1c80c50f5bef114f325a
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-25 16:23:40 +0100

    Update Header
    images

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -2,25 +2,51 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {
- FaArrowAltCircleLeft, FaArrowCircleRight
-} from "react-icons/fa";
+import image from "../../Constant/image";
+import { FaArrowAltCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
-const Header = ({}) => {
-    return (
-      <Container>
-        <Row>
-          <Col md={6}>1 of 3</Col>
+const Header = ({ }) => {
+ // const [currentImg, setCurrentImg] = useState(0);
+  //const length = slides.length;
 
-          <Col md={6}>
-            <section className="slider">
-              <FaArrowAltCircleLeft className="left-arrow" />
-              <FaArrowCircleRight className="right-arrow" />
-            </section>
-          </Col>
-        </Row>
-      </Container>
-    );
-}
+  //const nextSlide = () => {
+   //setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1);
+  //};
+
+  //const prevSlide = () => {
+    //setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1);
+ // };
+
+  //if (!Array.isArray(slides) || slides.length <= 0) {
+    //return null;
+ // }
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>1 of 3</Col>
+
+        <Col md={6}>
+          <section className="slider">
+            <FaArrowAltCircleLeft className="left-arrow"  />
+            <FaArrowCircleRight className="right-arrow" />
+            {[
+              image.slide1,
+              image.slide2,
+              image.slide3,
+              image.slide4,
+              image.slide5,
+              image.slide6,
+            ].map((slide, index) => {
+           
+            return <img src={slide} alt="travel image" className="image" />
+                 
+              
+            })}
+          </section>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Header;
---
+++ src/Constant/image.js
@@ -0,0 +1,15 @@
+import slide1 from "../Assets/slide-1.jpeg";
+import slide2 from "../Assets/slide-2.jpeg";
+import slide3 from "../Assets/slide-3.jpeg";
+import slide4 from "../Assets/slide-4.jpeg";
+import slide5 from "../Assets/slide-5.jpeg";
+import slide6 from "../Assets/slide-6.jpeg";
+
+export default {
+slide1,
+slide2,
+slide3,
+slide4,
+slide5,
+slide6
+};
