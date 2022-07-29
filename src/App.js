63d2107f163ad445d0c178e735b68a1e3bcc0190
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-29 13:32:50 +0100

    Update
    Browser Route

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Hpage from "./Pages/Hpage/Hpage";
 import Navbar from "./Components/navbar/Navbar";
 import React from "react";
-import Header from "./Components/header/Header";
-import Single from "./Pages/singlepage/Single";
-import Writing from "./Pages/writing/Writing";
-import Setting from "./Pages/settings/Settings";
-import Login from "./Pages/login/Login";
+
+///import Single from "./Pages/singlepage/Single";
+///import Writing from "./Pages/writing/Writing";
+///import Setting from "./Pages/settings/Settings";
+///import Login from "./Pages/login/Login";
 import Register from "./Pages/register/Register";
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+      
+        <Routes>
+          <Route path="/" element={<Hpage />}>
+            {" "}
+          </Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+
       <div>Zahra Olanrewaju</div>
-<Navbar/>
-<Header/>
-<Hpage />
-<Single />
-<Writing />
-<Setting />
-<Login />
-<Register />
     </div>
   );
 }
--- src/Pages/Hpage/Hpage.js
+++ src/Pages/Hpage/Hpage.js
@@ -4,18 +4,23 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Posts from "../../Components/post/Posts";
 import Sidebar from "../../Components/sidebar/Sidebar";
+import Header from "../../Components/header/Header";
+
 
 
 const Hpage = () => {
   return (
     <Container>
       <Row>
-        <Col  md={8}>
+        <Header />
+      </Row>
+      <Row>
+        <Col md={8}>
           <Posts />
         </Col>
 
-        <Col  md={4}>
-        <Sidebar/>
+        <Col md={4}>
+          <Sidebar />
         </Col>
       </Row>
     </Container>
