988f873962b11b56eedfb7134b1c05014525a111
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-12-12 13:30:03 +0000

    Update

--- src/Pages/singlepage/Single.js
+++ src/Pages/singlepage/Single.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import SinglePost from "../../Components/singlePost/SinglePost";
+import SinglePost from "../../Components/singlesPosts/SinglePost";
 import Sidebar from "../../Components/sidebar/Sidebar";
 
 const Single = () => {
